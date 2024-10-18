9f5e20578a251863c3b6fc7de84a6fc1df72b127
TJTJ-cpu <117895770+TJTJ-cpu@users.noreply.github.com>
2024-10-18 17:52:15 +0200

    Added Display func

--- GoDoList/ToDoList.go
+++ GoDoList/ToDoList.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -10,28 +10,42 @@ import (
 var Path string
 
 func main() {
-  GetCurrentPath()
+	GetCurrentPath()
 	CheckFileExistance()
 	MainMenu()
 }
 
-var bIsValid bool
+var (
+	bIsValid bool
+	bNotQuit bool = true
+)
 
 func MainMenu() {
-	var bNotQuit bool = true
 	for bNotQuit {
 
 		// Clear the screen
-		fmt.Print("\033[H\033[2J")
+		ClearScreen()
 		PrintMainMenu()
-		fmt.Println("Bool: ", bIsValid)
 
 		// Get user input
 		var userInput string
 		fmt.Scan(&userInput)
-		// reader := bufio.NewReader(os.Stdin)
-		// sentence, _ := reader.ReadString('\n')
-		IsValidInput(userInput, &bIsValid)
+		SwitchScene(userInput)
+	}
+}
+
+func SwitchScene(userInput string) {
+	IsValidInput(userInput, &bIsValid)
+	if bIsValid == false {
+		fmt.Println("Bool: ", bIsValid)
+		return
+	}
+	if userInput == "q" {
+		os.Exit(0)
+	} else if userInput == "1" {
+		DisplayFile()
+	} else if userInput == "2" {
+		AddToList()
 	}
 }
 
@@ -56,7 +70,7 @@ func CheckFileExistance() {
 	}
 }
 
-// is valid input
+// IS VALID INPUT
 var ValidInput = [6]string{"1", "2", "3", "4", "5", "q"}
 
 func IsValidInput(Input string, bIsValid *bool) {
@@ -70,14 +84,81 @@ func IsValidInput(Input string, bIsValid *bool) {
 	*bIsValid = false
 }
 
-func GetCurrentPath(){
-  fileName := "\\File.txt"
-  myDir, err := os.Getwd()
-  if (err != nil){
-    fmt.Println(err)
-  }
-  Path = myDir + fileName
-  fmt.Print("The path: ",Path)
+func GetCurrentPath() {
+	fileName := "\\File.txt"
+	myDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	Path = myDir + fileName
+}
+
+func AddToList() {
+	// Wait for enter
+	Continue("Enter your to do list: ")
+	reader := bufio.NewReader(os.Stdin)
+	sentence, _ := reader.ReadString('\n')
+
+	// Remove the ^m
+	sentence = string([]rune(sentence))[:len(sentence)-1]
+	sentence += "\n"
+	file, err := os.OpenFile("File.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	if _, err = file.WriteString(sentence); err != nil {
+		log.Fatal(err)
+	}
+    // Wait for user input
+	read2 := bufio.NewReader(os.Stdin)
+	_, _ = read2.ReadString('\n')
+	reader2 := bufio.NewReader(os.Stdin)
+	_, err2 := reader2.ReadString('\n')
+	if err2 != nil {
+		log.Fatal(err)
+	}
+}
+
+func DisplayFile() {
+	file := ReadFile()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	ClearScreen()
+	println("This is your to do lists")
+	for i := 0; i < len(lines); i++ {
+		fmt.Print(i+1, ": ", lines[i], "\n")
+	}
+	Continue("Press anything to continue...")
+}
+
+func Continue(output string) {
+	// ClearScreen()
+	println(output)
+	read2 := bufio.NewReader(os.Stdin)
+	_, _ = read2.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)
+	_, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ClearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func ReadFile() *os.File {
+	file, err := os.Create("File.txt")
+	fmt.Print("File created")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	return file
 }
 
 // Icon
